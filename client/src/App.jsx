91fff99113118aac37c8b2ebbaf821eb7a92d2d4
seungmin lee <sorpwjdafh@naver.com>
2022-08-10 12:33:31 +0900

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Header.css";
+
+const TextDropdown = (props) => {
+  return (
+    <div>
+      <span className="text bold">{props.text}</span>
+      <span>
+        <img
+          src="https://klayswap.com/img/icon/ic-chevron-bottom-disable-gray.svg"
+          alt="chevron"
+        />
+      </span>
+    </div>
+  );
+};
+
+const Network = () => {
+  return (
+    <div className="network">
+      <span className="ic-status pointer green"></span>
+      <span className="text bold">mainnet</span>
+      <span className="green sharp bold">#</span>
+      <span className="text2">98,203,572</span>
+    </div>
+  );
+};
+
+const Header = () => {
+  return (
+    <div className="header">
+      <TextDropdown text="HELP" />
+      <Network />
+      <TextDropdown text="KO" />
+    </div>
+  );
+};
+
+export default Header;
